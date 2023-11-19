49edf92f1857236cf67a4d86b2e52a7f16005406
Poldro <valmori.paolo@gmail.com>
2023-11-19 22:19:49 +0100

    ga4

--- src/lib/components/Analytics/Analytics.svelte
+++ src/lib/components/Analytics/Analytics.svelte
@@ -1,3 +1,35 @@
 <svelte:head>
-  <script async src="https://analytics.eu.umami.is/script.js" data-website-id="5109ace0-7dd6-410a-9cf2-90e523d772f4"></script>
+	<script
+		async
+		src="https://analytics.eu.umami.is/script.js"
+		data-website-id="5109ace0-7dd6-410a-9cf2-90e523d772f4"
+	></script>
+
+  
+	<!-- Google Tag Manager -->
+	<script>
+		(function (w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-P2WPCZKM');
+	</script>
+	<!-- End Google Tag Manager -->
+
 </svelte:head>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript
+		><iframe
+			title="gtag"
+			src="https://www.googletagmanager.com/ns.html?id=GTM-P2WPCZKM"
+			height="0"
+			width="0"
+			style="display:none;visibility:hidden"
+		/></noscript
+	>
+	<!-- End Google Tag Manager (noscript) -->
