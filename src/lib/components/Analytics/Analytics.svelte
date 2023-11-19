50d0df9ad8d82db2853f7ce091611c9a48431876
Poldro <valmori.paolo@gmail.com>
2023-11-19 21:02:18 +0100

    fixed gtag type

--- src/lib/components/Analytics/Analytics.svelte
+++ src/lib/components/Analytics/Analytics.svelte
@@ -1,30 +1,31 @@
-<script lang='ts'>
-	import { onMount } from 'svelte';
-	import { page } from '$app/stores';
+<script lang="ts">
+  import { page } from '$app/stores'
 
-	onMount(() => {
-		// Initialize GTM once
-		if (typeof window !== 'undefined') {
-			(function(w, d, s, l, i) {
-				w[l] = w[l] || [];
-				w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
-				var f = d.getElementsByTagName(s)[0],
-					j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-				j.async = true;
-				j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-				f.parentNode.insertBefore(j, f);
-			})(window, document, 'script', 'dataLayer', 'GTM-P2WPCZKM');
-		}
-	});
+  $: {
+    if (typeof gtag !== 'undefined') {
+      gtag('config', 'GTM-P2WPCZKM', {
+        page_title: document.title,
+        page_path: $page.url.pathname,
+      })
+    }
+  }
 
-	$: {
-		// Track page views on route change
-		if (typeof window !== 'undefined' && typeof window.dataLayer !== 'undefined') {
-			window.dataLayer.push({
-				'event': 'pageview',
-				'pagePath': $page.url.pathname,
-				'pageTitle': document.title
-			});
-		}
-	}
+
 </script>
+
+<svelte:head>
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=GTM-P2WPCZKM">
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || []
+
+    function gtag() {
+      dataLayer.push(arguments)
+    }
+
+    gtag('js', new Date())
+    gtag('config', 'GTM-P2WPCZKM')
+  </script>
+</svelte:head>
