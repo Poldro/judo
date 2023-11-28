79cf5e2d40bce19b7110cf8a28eb2921109180e2
Poldro <valmori.paolo@gmail.com>
2023-11-28 16:14:12 +0100

    gtm

--- src/lib/components/Analytics/Analytics.svelte
+++ src/lib/components/Analytics/Analytics.svelte
@@ -5,16 +5,29 @@
 		data-website-id="5109ace0-7dd6-410a-9cf2-90e523d772f4"
 	></script>
 
-	<!-- Google tag (gtag.js) -->
-	<!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-PV28XWLV6J"></script>
+	<!-- Google Tag Manager -->
 	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-		gtag('js', new Date());
-
-		gtag('config', 'G-PV28XWLV6J');
+		(function (w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-P2WPCZKM');
 	</script>
+	<!-- End Google Tag Manager -->
 </svelte:head>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript
+	><iframe
+		src="https://www.googletagmanager.com/ns.html?id=GTM-P2WPCZKM"
+		height="0"
+		width="0"
+		style="display:none;visibility:hidden"
+	/></noscript
+>
+<!-- End Google Tag Manager (noscript) -->
