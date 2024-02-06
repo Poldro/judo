99bd93af79d3204e24abc71cf849928ccf808136
Poldro <valmori.paolo@gmail.com>
2024-02-06 22:32:18 +0100

    change back

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,7 +1,7 @@
 import directus from "$lib/directus";
 import { readItems } from "@directus/sdk";
 
-let date = new Date().toISOString().split('T')[0];
+let date = new Date().toISOString().split('T')[0]
 
 export async function GET({ setHeaders }) {
     setHeaders({
@@ -12,38 +12,41 @@ export async function GET({ setHeaders }) {
 
     const url_techniques = await directus.request(readItems('techniques', {
         fields: ['slug']
-    }));
+    }))
 
     const url_katas = await directus.request(readItems('kata', {
         fields: ['slug']
-    }));
+    }))
 
     const url_exams = await directus.request(readItems('exams', {
         fields: ['slug']
-    }));
+    }))
 
     const url_programs = await directus.request(readItems('programs_exam', {
         fields: ['slug']
-    }));
+    }))
 
-    // Validate fetches
-    if (!url_techniques || url_techniques.length <= 0 || !url_katas || url_katas.length <= 0 || !url_exams || url_exams.length <= 0 || !url_programs || url_programs.length <= 0) {
-        throw new Error('Failed to fetch urls.');
+    if (url_techniques.length <= 0) {
+        throw new Error('Failed to fetch url_techniques.');
+    }
+    if (url_katas.length <= 0) {
+        throw new Error('Failed to fetch url_katas.');
+    }
+    if (url_exams.length <= 0) {
+        throw new Error('Failed to fetch url_exams.');
+    }
+    if (url_programs.length <= 0) {
+        throw new Error('Failed to fetch url_programs.');
     }
 
     function generateExamProgramPaths() {
         let paths: string[] = [];
         url_exams.forEach(exam => {
             url_programs.forEach(program => {
-                paths.push(`
-<url>
-    <loc>${site}/esami/${exam.slug}/programmi/${program.slug}</loc>
-    <changefreq>weekly</changefreq>
-    <lastmod>${date}</lastmod>
-</url>`);
+                paths.push(`${site}/esami/${exam.slug}/programmi/${program.slug}`);
             });
         });
-        return paths.join('');
+        return paths;
     }
 
     const examProgramPaths = generateExamProgramPaths();
@@ -51,28 +54,27 @@ export async function GET({ setHeaders }) {
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url>
-    <loc>${site}</loc>
-    <changefreq>daily</changefreq>
-    <priority>0.7</priority>
+<loc>${site}</loc>
+<changefreq>daily</changefreq>
+<priority>0.7</priority>
 </url>
 ${url_techniques.map(item => `
 <url>
-    <loc>${site}/tecniche/${item.slug}</loc>
-    <changefreq>weekly</changefreq>
-    <lastmod>${date}</lastmod>
-</url>`).join('')}
+<loc>${site}/tecniche/${item.slug}</loc>
+<changefreq>weekly</changefreq>
+<lastmod>${date}</lastmod>
+</url>
+`).join('')}
+
 ${url_katas.map(item => `
 <url>
-    <loc>${site}/kata/${item.slug}</loc>
-    <changefreq>weekly</changefreq>
-    <lastmod>${date}</lastmod>
-</url>`).join('')}
+<loc>${site}/kata/${item.slug}</loc>
+<changefreq>weekly</changefreq>
+<lastmod>${date}</lastmod>
+</url>
+`).join('')}
+
 ${examProgramPaths}
 </urlset>`;
-
-    return new Response(sitemap, {
-        headers: {
-            'Content-Type': 'application/xml'
-        }
-    });
+    return new Response(sitemap);
 }
